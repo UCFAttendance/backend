75d514ee941fa649619fc38ae70670d71345955d
Hoan Tran <xhoantran@gmail.com>
2024-11-22 21:23:16 -0500

    Assign public ip

--- terraform/service.tf
+++ terraform/service.tf
@@ -60,7 +60,7 @@ resource "aws_ecs_service" "attendance_backend" {
   network_configuration {
     subnets          = data.terraform_remote_state.core-infra.outputs.public-subnet-ids
     security_groups  = [aws_security_group.service_lb_sg.id]
-    assign_public_ip = false
+    assign_public_ip = true
   }
 
   load_balancer {
