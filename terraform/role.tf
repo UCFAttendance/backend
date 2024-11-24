77d757a5890476d319f003c47a957bef6b06244e
Hoan Tran <xhoantran@gmail.com>
2024-11-23 22:53:29 -0500

    Attach policy to ecs role

--- terraform/role.tf
+++ terraform/role.tf
@@ -80,3 +80,8 @@ resource "aws_iam_policy" "rds_access_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "rds_access_policy_attachment" {
+  role       = aws_iam_role.backend_execution_role.name
+  policy_arn = aws_iam_policy.rds_access_policy.arn
+}
