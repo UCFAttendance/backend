9af1baadf04c6a682af99af261dee2df26cbe500
Hoan Tran <xhoantran@gmail.com>
2024-11-24 16:24:07 -0500

    Update load balancer port

--- terraform/service.tf
+++ terraform/service.tf
@@ -90,7 +90,7 @@ resource "aws_ecs_service" "attendance_backend" {
   load_balancer {
     target_group_arn = aws_lb_target_group.app_target_group.arn
     container_name   = "${local.app_prefix}-container"
-    container_port   = 80
+    container_port   = 5000
   }
 
   lifecycle {
