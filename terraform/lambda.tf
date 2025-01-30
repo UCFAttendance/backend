8470937c9c7f9163b5b78e23b6e80c7228289094
Hoan Tran <xhoantran@gmail.com>
2025-01-30 14:58:04 -0500

    Update package type

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,6 +4,7 @@ resource "aws_lambda_function" "attendance_migration" {
   image_uri     = "${data.aws_ecr_repository.attendance_backend.repository_url}:${var.image_tag}"
   memory_size   = 256
   timeout       = 300
+  package_type  = "Image"
   vpc_config {
     subnet_ids         = data.terraform_remote_state.core-infra.outputs.public-subnet-ids
     security_group_ids = [aws_security_group.service_lb_sg.id]
