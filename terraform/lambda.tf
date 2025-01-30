f0ea467429e8895286a4d3d30363f87718cb9959
Hoan Tran <xhoantran@gmail.com>
2025-01-30 18:28:15 -0500

    Update CMD for lambda migration

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "attendance_migration" {
     security_group_ids = [aws_security_group.service_lb_sg.id]
   }
   image_config {
-    command = ["migrate.handler"]
+    command           = ["migrate.handler"]
     working_directory = "/app"
   }
   environment {
