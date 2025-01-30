bc7da007a8248723201cbefcfcdd3abb0c716fa2
Hoan Tran <xhoantran@gmail.com>
2025-01-30 15:31:13 -0500

    Update lambda role

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "attendance_migration" {
   function_name = "attendance_migration"
-  role          = aws_iam_role.backend_task_role.arn
+  role          = aws_iam_role.lambda_migration_role.arn
+  package_type  = "Image"
   image_uri     = "${data.aws_ecr_repository.attendance_backend.repository_url}:${var.image_tag}"
   memory_size   = 256
   timeout       = 300
-  package_type  = "Image"
   vpc_config {
     subnet_ids         = data.terraform_remote_state.core-infra.outputs.public-subnet-ids
     security_group_ids = [aws_security_group.service_lb_sg.id]
