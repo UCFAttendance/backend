f1e22b6f1af40a8cbc6c0b61c85432caef0b538c
Hoan Tran <xhoantran@gmail.com>
2025-03-11 21:15:37 -0400

    feat: register core tables

--- attendance/core/admin.py
+++ attendance/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Attendance, Course, Session
+
 # Register your models here.
+admin.site.register(Attendance)
+admin.site.register(Session)
+admin.site.register(Course)
